73998071237ed8dd839af73a5e97a875722167aa
ejlee <sssver28@gmail.com>
2021-08-05 18:07:34 +0900

    promise all

--- 1.split_slice.js
+++ 1.split_slice.js
@@ -3,3 +3,6 @@ let ins2 = 'https://www.instagram.com/test1/, https://www.instagram.com/test2/'
 
 console.log(ins1.split(', ').map(el => el.slice(26, -1))) // ['abcde]
 console.log(ins2.split(', ').map(el => el.slice(26, -1))) // [ 'test1', 'test2' ]
+
+// split 특정 문자로 구분해서 배열생성
+// slice 텍스트 자르기
---
+++ 2.promiseAll.js
@@ -0,0 +1,32 @@
+async function test(text, time) {
+	return new Promise((resolved, rejected) => {
+		setTimeout(
+			() =>
+				typeof text === 'string'
+					? resolved('문자열 들어옴')
+					: rejected('문자열 아님'),
+			time
+		)
+	})
+}
+
+// console.time("소요시간")
+// await test("3초", 3000)
+// await test("2초", 2000)
+// await test("1초", 1000)
+// console.timeEnd("소요시간") // 6.005s
+
+// ! promise.all
+// 순서가 보장되지 않아도 되는 상황에서, 병렬로 처리할 수 있다.
+
+// console.time("소요시간")
+// await Promise.all([
+// 	test("3초", 3000),
+// 	test("2초", 2000),
+// 	test("1초", 1000)
+// ])
+// console.timeEnd("소요시간") // 3.003s
+
+// 비동기 처리가 실패했을 경우, 즉시 에러를 반환한다.
+
+// 참고 : https://code-masterjung.tistory.com/91
