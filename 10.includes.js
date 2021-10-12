a665c375a8b8ac98e31298ab69b2e002a3f17854
ejlee <sssver28@gmail.com>
2021-10-12 14:35:04 +0900

    includes

---
+++ 10.includes.js
@@ -0,0 +1,6 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.includes("Mango"))   // Returns true
+console.log(fruits.includes("Orange"))   // Returns true
+console.log(fruits.includes("Apple"))   // Returns true
+console.log(fruits.includes("Banana"))   // Returns true
+console.log(fruits.includes("Water"))   // Returns false
