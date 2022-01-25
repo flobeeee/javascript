5444823fdc220e43543ed49acd3416350d2a94e5
ejlee <sssver28@gmail.com>
2022-01-25 10:15:48 +0900

    booleand

---
+++ 19.boolean.js
@@ -0,0 +1,13 @@
+const filters = {"isDisplay": false}
+
+Object.entries(filters).forEach(([key, value]) => {
+  if (typeof value === 'boolean') {
+    if (key === 'isDisplay') {
+      console.log(value)
+    }
+  }
+
+  if (value) {
+    console.log(value) // 값이 false인 경우 넘어오지 않음.
+  }
+})
