793c78190e129c3aa35220035723fc76673b6b9d
ejlee <ejlee@mediance.co.kr>
2022-07-04 17:14:50 +0900

    객체에서 데이터 가공

---
+++ 26.extractData.js
@@ -0,0 +1,328 @@
+const data = {
+  "kind": "youtube#videoCategoryListResponse",
+  "etag": "wiISN0wuAFKfWlYe9SoTlvD2fV0",
+  "items": [
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "qlv8uOCGI38ZKFxPPdC1oHiPj5g",
+      "id": "1",
+      "snippet": {
+        "title": "영화/애니메이션",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "usy1wrFHZr2SY4ntwc-kGLZmLls",
+      "id": "2",
+      "snippet": {
+        "title": "자동차",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "cpwduYUMd1I9JzDRI4qeAb60j3Q",
+      "id": "10",
+      "snippet": {
+        "title": "음악",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "IqCB51gUUfoCWV2prb64vnGGqOI",
+      "id": "15",
+      "snippet": {
+        "title": "반려동물/동물",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "4naHDC8zLbKM0Ju1lrxlKU_552c",
+      "id": "17",
+      "snippet": {
+        "title": "스포츠",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "_vv39Yy6iDRaRV-swEE4aJqPIfA",
+      "id": "18",
+      "snippet": {
+        "title": "단편 영화",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "m07RpjyjC4--QZoFrqg1Jrm_0HI",
+      "id": "19",
+      "snippet": {
+        "title": "여행/이벤트",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "sHpGlMwEwN-uO5D0R3u6sL-KTnA",
+      "id": "20",
+      "snippet": {
+        "title": "게임",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "YT5M4xDYhzyt6r0Tb0DO5CqehuA",
+      "id": "21",
+      "snippet": {
+        "title": "동영상 블로그",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "zPQny_LGT3pgp_Xw7D3CWQbyjPQ",
+      "id": "22",
+      "snippet": {
+        "title": "인물/블로그",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "JwaSYGg1rHDIWK2zhOfsONB3ogw",
+      "id": "23",
+      "snippet": {
+        "title": "코미디",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "ma9ExnEMQKXuCJAufVkbLIjn6zk",
+      "id": "24",
+      "snippet": {
+        "title": "엔터테인먼트",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "LgkuqUodUgouqsue-JO9BDdfJrg",
+      "id": "25",
+      "snippet": {
+        "title": "뉴스/정치",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "GsWn--eAw5d5XILWgOUCFPCI72E",
+      "id": "26",
+      "snippet": {
+        "title": "노하우/스타일",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "MsWfOuywwmAbR5XsFQAhSUJISY0",
+      "id": "27",
+      "snippet": {
+        "title": "교육",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "nrE93_0sPxJ4Ghi4X5BKBAo_nV4",
+      "id": "28",
+      "snippet": {
+        "title": "과학기술",
+        "assignable": true,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "5Jq1UN7sIlFcXe4dbRMTVbZsxp0",
+      "id": "30",
+      "snippet": {
+        "title": "영화",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "6bqw5jHFZqDl7PiFGKjAHWgipkg",
+      "id": "31",
+      "snippet": {
+        "title": "애니메/애니메이션",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "iVqfzc-E4PQ-YPzbF90BIEEUlZw",
+      "id": "32",
+      "snippet": {
+        "title": "액션/모험",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "GWnZh6O1Qv7vwnd34Uj97_DT5S0",
+      "id": "33",
+      "snippet": {
+        "title": "고전",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "3VtqQrgjoSnGtrmcgNDNj6nNWkQ",
+      "id": "34",
+      "snippet": {
+        "title": "코미디",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "enKQG59LRlkHwDwPQ1W2mASjeFE",
+      "id": "35",
+      "snippet": {
+        "title": "다큐멘터리",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "jr7V1R2f4_EYcYMtZUOXg3-tFRc",
+      "id": "36",
+      "snippet": {
+        "title": "드라마",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "KtcR14K0sJ72CMasVWWVeCrrJaQ",
+      "id": "37",
+      "snippet": {
+        "title": "가족",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "UBRc-lifziMX9QmfzdsvdeIMnJk",
+      "id": "38",
+      "snippet": {
+        "title": "외국",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "oha4SNB7xeEg_Qdx_-y_301YITA",
+      "id": "39",
+      "snippet": {
+        "title": "공포",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "8S34MWksMi4vcC2xYTBBYEsmnEk",
+      "id": "40",
+      "snippet": {
+        "title": "SF/판타지",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "EQfRRnVNYG2F6BOsir5t8qT8ALI",
+      "id": "41",
+      "snippet": {
+        "title": "스릴러",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "mSgAXHd-YL9mFq7MzLOoeyDRsUc",
+      "id": "42",
+      "snippet": {
+        "title": "단편",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "c7-D4YrYyTvd-LB_qDWWDSDSrys",
+      "id": "43",
+      "snippet": {
+        "title": "프로그램",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    },
+    {
+      "kind": "youtube#videoCategory",
+      "etag": "rk_KZ00hDhe55DetJA26y3dH98w",
+      "id": "44",
+      "snippet": {
+        "title": "예고편",
+        "assignable": false,
+        "channelId": "UCBR8-60-B28hp2BmDPdntcQ"
+      }
+    }
+  ]
+}
+
+function extract(data) {
+  let extractData = {}
+  
+  data.items.map(el => {
+    return extractData[el.id] = el.snippet.title
+  })
+
+  return extractData
+}
+
+console.log(extract(data))
