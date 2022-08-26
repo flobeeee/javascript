b3f78e8907ade6a503c54b5cc2099eda62fe0d9b
ejlee <ejlee@mediance.co.kr>
2022-08-26 17:08:43 +0900

    특수문자 제거 정규식

--- 6.regex.js
+++ 6.regex.js
@@ -1,3 +1,30 @@
+// 3. 특정 특수문자 지우기
+
+let hastag = '#코딩'
+console.log(hastag.replace(/\#/g,'')) // 코딩
+hastag = '코#딩###'
+console.log(hastag.replace(/\#/g,'')) // 코딩
+hastag = '####코#딩###'
+console.log(hastag.replace(/\#/g,'')) // 코딩
+const account = '@star'
+console.log(account.replace(/\@/g,'')) // star
+
+
+function regExp(str){  
+  var reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi
+  //특수문자 검증
+  if(reg.test(str)){
+    //특수문자 제거후 리턴
+    return str.replace(reg, "");    
+  } else {
+    //특수문자가 없으므로 본래 문자 리턴
+    return str;
+  }  
+}
+
+console.log(regExp(hastag))
+
+// ---
 let word = '안녕 dsl1233'
 
 
