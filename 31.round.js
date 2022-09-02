6488bf0e2494758d34df1ce135d34eef63e43e23
ejlee <ejlee@mediance.co.kr>
2022-09-02 15:34:31 +0900

    반올림

---
+++ 31.round.js
@@ -0,0 +1,23 @@
+// 큰 숫자에 소수점 넣어서 반올림
+
+const insight = {
+  metrics: {
+    costMicros: '13179934757', // 13180으로 변환
+    averageCpc: 1098327896.4166667,
+    impressions: '15427'
+  }
+}
+
+let campaign = {
+  adCost: 0
+}
+
+if (insight) {
+  const adCost = insight.metrics.costMicros
+
+  const newAdcost = `${adCost.slice(0,-6)}.${adCost.slice(-6)}`
+
+  campaign.adCost = Math.round(newAdcost)
+}
+
+console.log(campaign) // { adCost: 13180 }
