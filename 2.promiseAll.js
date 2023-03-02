43f3368678e9144933c1e35d6016a225943ab00e
Eunjeong Lee <ejlee@qedgolf.com>
2023-03-02 10:50:52 +0900

    await promise.all

--- 2.promiseAll.js
+++ 2.promiseAll.js
@@ -18,13 +18,22 @@ async function test (text, time) {
 // ! promise.all
 // 순서가 보장되지 않아도 되는 상황에서, 병렬로 처리할 수 있다.
 
-// console.time("소요시간")
-// await Promise.all([
-// 	test("3초", 3000),
-// 	test("2초", 2000),
-// 	test("1초", 1000)
-// ])
-// console.timeEnd("소요시간") // 3.003s
+console.time("소요시간")
+await Promise.all([
+	test("3초", 3000),
+	test("2초", 2000),
+	test("1초", 1000)
+])
+console.timeEnd("소요시간") // 3.003s
+
+// await 안붙이는 경우 -> 쓰지말기
+console.time("소요시간1")
+Promise.all([
+	test("3초", 3000),
+	test("2초", 2000),
+	test("1초", 1000)
+])
+console.timeEnd("소요시간1") // 0.074s
 
 // 비동기 처리가 실패했을 경우, 즉시 에러를 반환한다.
 
