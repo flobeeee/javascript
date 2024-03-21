488d0e393b33e72c4f7563ac7c2e42265be4df58
Eunjeong Lee <ejlee@qedgolf.com>
2024-03-21 09:46:41 +0900

    moment-timezone

--- 39.moment.js
+++ 39.moment.js
@@ -1,4 +1,4 @@
-import moment from 'moment'
+import moment from 'moment-timezone'
 
 let now = moment() // 현재
 console.log(now.format('YYYY-MM-DD')) // 2023-05-03
@@ -12,13 +12,12 @@ console.log(now.day(0)) // 같은 주차 일요일
 console.log(now.day(6)) // 같은 주차 토요일
 
 
-const arr = ['1234', '5678']
-
-console.log(arr.join(' '))
-
-const date = moment(1698134400000).format('YYYY년 MM월 DD일 HH:mm:ss');
+const date = moment(1704499140000).format('YYYY년 MM월 DD일 HH:mm:ss');
 console.log(date); // 2023년 07월 20일 13:40:00 
-const date1 = moment(1698135000000).format('YYYY년 MM월 DD일 HH:mm:ss');
+const date1 = moment(1704496500000).format('YYYY년 MM월 DD일 HH:mm:ss');
 console.log(date1); // 2023년 07월 20일 13:56:40
+const date2 = moment(1702665720000).format('YYYY년 MM월 DD일 HH:mm:ss');
+console.log(date2); // 2023년 07월 20일 13:56:40
+console.log(moment('20231225 163000', 'YYYYMMDD HHmm').unix() + '000')
 
-console.log(moment('20231028 00100000', 'YYYYMMDD HHmm').unix() + '000')
+
